70b45d286fc266cd24b32e356bc3cb737ccc4190
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2020-03-25 08:04:26 -0300

    fix indent by rubocop

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -1,9 +1,8 @@
-require "serverspec"
-require "docker"
-
+require 'serverspec'
+require 'docker'
 
 # Setting up docker image
-describe "Dockerfile" do
+describe 'Dockerfile' do
   before(:all) do
     @image = Docker::Image.build_from_dir('.')
     @image.tag(repo: 'ubuntu-teste', tag: 'novo')
@@ -11,50 +10,46 @@ describe "Dockerfile" do
     set :os, family: :ubuntu
     set :backend, :docker
     set :docker_image, @image.id
-    set :docker_container_create_options, { 'Entrypoint' => ['bash']}
+    set :docker_container_create_options, { 'Entrypoint' => ['bash'] }
   end
 
-# Image infrastructure tests
-  it "should have the maintaner label" do
-    expect(@image.json['Config']["Labels"].has_key?('maintainer'))
+  # Image infrastructure tests
+  it 'should have the maintaner label' do
+    expect(@image.json['Config']['Labels'].key?('maintainer'))
   end
 
-  it "should be linux Os type" do
-    expect(@image.json['Os'].eql? 'linux')
+  it 'should be linux Os type' do
+    expect @image.json['Os'].eql? 'linux'
   end
 
-  it "should has no user" do
+  it 'should has no user' do
     expect(@image.json['Config']['User'].nil?)
   end
 
-  it "should has repo tags" do
-    expect(@image.json['RepoTags'].include? ('ubuntu-teste:novo'))
+  it 'should has repo tags' do
+    expect(@image.json['RepoTags'].include?('ubuntu-teste:novo'))
   end
 
-  it "should has port 80 exposed" do
-    expect(@image.json['ContainerConfig']['ExposedPorts'].has_key? ("80/tcp")).to be_truthy
+  it 'should has port 80 exposed' do
+    expect(@image.json['ContainerConfig']['ExposedPorts'].key?('80/tcp')).to be
   end
 
-# Container applications and files tests
-  [
-    'vim',
-    'nginx',
-    'httpd'
-  ].each do |app|
+  # Container applications and files tests
+  %w[vim nginx httpd].each do |app|
     describe package(app) do
-      it {should be_installed}
+      it { should be_installed }
     end
   end
 
   describe port(80) do
-    it {should be_listening.with('tcp')}
+    it { should be_listening.with('tcp') }
   end
 
   describe command('vim --help') do
-    its(:exit_status) {should eq 0}
+    its(:exit_status) { should eq 0 }
   end
 
   describe command('cat /usr/share/nginx/html/index.html') do
-    its(:stdout) {should not_contain('Welcome to')}
+    its(:stdout) { should not_contain('Welcome to') }
   end
 end
